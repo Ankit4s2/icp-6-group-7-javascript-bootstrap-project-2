f77962e003d15c06f293f3be107486290d94358e
Aashika Jain <jainaashika1510@gmail.com>
2023-12-17 16:06:16 +0530

    children file added

---
+++ javascript/children.js
@@ -0,0 +1,53 @@
+function fun(){
+    
+}
+function show(arg) {
+    if (arg ===1) {
+        var imageElement = document.getElementById("pic");;
+        var imageUrl = "../images/children-img/img4.jpg";
+        imageElement.src = imageUrl;
+        document.getElementById("blk").style.display="block";
+    }
+    if (arg ===2) {
+        var imageElement = document.getElementById("pic");;
+        var imageUrl = "../images/children-img/img5.webp";
+        imageElement.src = imageUrl;
+        document.getElementById("blk").style.display="block";
+    }
+    if (arg ===3) {
+        var imageElement = document.getElementById("pic");;
+        var imageUrl = "../images/children-img/img6.webp";
+        imageElement.src = imageUrl;
+        document.getElementById("blk").style.display="block";
+    }
+    if (arg ===4) {
+        var imageElement = document.getElementById("pic");;
+        var imageUrl = "../images/children-img/img7.jpg";
+        imageElement.src = imageUrl;
+        document.getElementById("blk").style.display="block";
+    }
+    if (arg ===5) {
+        var imageElement = document.getElementById("pic");;
+        var imageUrl = "../images/children-img/img8.jpeg";
+        imageElement.src = imageUrl;
+        document.getElementById("blk").style.display="block";
+    }
+    if (arg ===6) {
+        var imageElement = document.getElementById("pic");;
+        var imageUrl = "../images/children-img/img9.webp";
+        imageElement.src = imageUrl;
+        document.getElementById("blk").style.display="block";
+    }
+    if (arg ===7) {
+        var imageElement = document.getElementById("pic");;
+        var imageUrl = "../images/children-img/img10.webp";
+        imageElement.src = imageUrl;
+        document.getElementById("blk").style.display="block";
+    }
+    if (arg ===8) {
+        var imageElement = document.getElementById("pic");;
+        var imageUrl = "../images/children-img/img11.webp";
+        imageElement.src = imageUrl;
+        document.getElementById("blk").style.display="block";
+    }
+}
